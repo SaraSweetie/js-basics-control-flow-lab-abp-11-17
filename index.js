cec9b8c7ada8e87b8bde787663331bb1eb0a5319
Sara Smith <sarasmith887@gmail.com>
2017-11-12 22:15:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 function scuberGreetingForFeet (distance){
   if (distance < 400 ) {
     return ('This one is on me!')
-  }  else if (distance >= 2000 && scuberGreetingForFeet <= 2499) {
+  }  else if (distance > 2000 && scuberGreetingForFeet <= 2499) {
     return ('I will gladly take your thirty bucks.')
   } else (distance > 2500 ) {
     return ('No can do.')

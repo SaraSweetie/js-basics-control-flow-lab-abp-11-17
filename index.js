53bbe31de88b86154bd3016d625f26c5a7c9bc4e
Sara Smith <sarasmith887@gmail.com>
2017-11-12 22:38:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ function switchOnCharmFromTip(tip) {
       return 'Thank you so much.'
     case "not as generous":
       return 'Thank you.'
-    case "generous" :
+    case "thanks for everything" :
       return 'Bye.'
         }
   }

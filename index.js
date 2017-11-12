d4d72293536f3f91f92df073a8ee72ea14fe7247
Sara Smith <sarasmith887@gmail.com>
2017-11-12 22:14:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // Write your code in this file!
-function scuberGreetingForFeet(distance){
+function scuberGreetingForFeettest (distance){
   if (distance <= 400 ) {
     return ('This one is on me!')
   }  else if (distance >= 2000 && scuberGreetingForFeet <= 2499) {

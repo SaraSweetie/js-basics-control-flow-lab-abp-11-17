cb3ed11034ad0cfcd9a3e9953d2874d9d88661ad
Sara Smith <sarasmith887@gmail.com>
2017-11-12 22:14:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 // Write your code in this file!
-function scuberGreetingForFeet(){
-  if (scuberGreetingForFeet <= 400 ) {
-    console.log ('This one is on me!')
-  }  else if (scuberGreetingForFeet >= 2000 && scuberGreetingForFeet <= 2499) {
-    console.log ('I will gladly take your thirty bucks.')
-  } else (scuberGreetingForFeet > 2500 ) {
-    console.log ('No can do.')
+function scuberGreetingForFeet(distance){
+  if (distance <= 400 ) {
+    return ('This one is on me!')
+  }  else if (distance >= 2000 && scuberGreetingForFeet <= 2499) {
+    return ('I will gladly take your thirty bucks.')
+  } else (distance > 2500 ) {
+    return ('No can do.')
   }
 }

d093c322867ce249cc035363ba9a68b9a00493bc
Sara Smith <sarasmith887@gmail.com>
2017-11-12 22:25:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ function scuberGreetingForFeet (distance){
 function ternaryCheckCity(city) {
   if (city === NYC) {
   return 'Ok, sounds good.'
-  if (city !== NYC) {
+  } if (city !== NYC) {
     return 'No go.'
   }
 }

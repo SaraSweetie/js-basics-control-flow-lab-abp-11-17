97fac20d2cc11a51dba86d5d8a6c4518f4046f83
Sara Smith <sarasmith887@gmail.com>
2017-11-12 22:09:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 function scuberGreetingForFeet(199){
   if (scuberGreetingForFeet <= 400 ) {
     console.log ('This one is on me!')
-  }  else if (scuberGreetingForFeet > 2000 ) {
+  }  else if (scuberGreetingForFeet >= 2000 && scuberGreetingForFeet <= 2499) {
     console.log ('I will gladly take your thirty bucks.')
   } else (scuberGreetingForFeet > 2500 ) {
     console.log ('No can do.')

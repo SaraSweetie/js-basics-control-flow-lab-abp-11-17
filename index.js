b4a9f547e3f628e536ee837f3364b5fd224a978d
Sara Smith <sarasmith887@gmail.com>
2017-11-12 22:01:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 // Write your code in this file!
 function scuberGreetingForFeettest(){
   if (scuberGreetingForFeettest <= 400 ) {
-    consol.log ()'This one is on me!')
+    console.log ()'This one is on me!')
+  }
+  else if (scuberGreetingForFeettest <= 400 ) {
+    
   }
 
 }
